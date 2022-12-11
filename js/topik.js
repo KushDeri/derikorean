bf1a6f6b6ac84740261aec7920e1900a39397f0f
KushDeri <97098215+KushDeri@users.noreply.github.com>
2022-12-11 19:13:42 +0100

    yes

---
+++ js/topik.js
@@ -0,0 +1,10 @@
+(function () {
+	let lokation = document.querySelector(".lokation");
+	let card = document.querySelector(".card");
+	//let MaxWidthLokation = card - lokation;
+
+	if (card + lokation > region) {}
+	calendar.insertAdjacentHTML("beforeend", `
+        <p>...</p>
+    `);
+}());
